00626e422b8be9e43f801460b4bf32c1e910e655
joaovictoralencar <joaovictoralencarpascoal@gmail.com>
2018-04-28 10:38:07 -0300

    Adição dos Pop Ups
    Adicionei um básico de pop up. Tipo, existe a janela, porém seu conteúdo ainda precisa ser definido. Coloquei nas telas de menu. To com algumas observações pra fazer sobre o site...

---
+++ js/application.js
@@ -0,0 +1,63 @@
+window.onload = function(){ 
+    //Pegando as referencisa pros pop ups
+	var popupPalitos = document.getElementById('popup_opcao1');
+	var popupEnigmas = document.getElementById('popup_opcao2');
+	var popupVideos = document.getElementById('popup_opcao3');
+	var popupJogos = document.getElementById('popup_opcao4');
+
+	//Pegando as referencias dos botões que abrirão os pop ups
+	var btnPalitos = document.getElementById("opcao1");
+	var btnEnigmas = document.getElementById("opcao2");
+	var btnVideos = document.getElementById("opcao3");
+	var btnJogos = document.getElementById("opcao4");
+
+	//Pegando as referencias dos botões x que fecharão os pop ups
+	var span0 = document.getElementsByClassName("close")[0];
+	var span1 = document.getElementsByClassName("close")[1];
+	var span2 = document.getElementsByClassName("close")[2];
+	var span3 = document.getElementsByClassName("close")[3];
+
+	//Funções que mostram os pop ups
+	btnPalitos.onclick = function() {
+	    popupPalitos.style.display = "block";
+	}
+	btnEnigmas.onclick = function() {
+	    popupEnigmas.style.display = "block";
+	}
+	btnVideos.onclick = function() {
+	    popupVideos.style.display = "block";
+	}
+	btnJogos.onclick = function() {
+	    popupJogos.style.display = "block";
+	}
+
+	//Quando o usuario clica em X, fecha o pop up
+	span0.onclick = function() {
+	    popupPalitos.style.display = "none";
+	}
+	span1.onclick = function() {
+	    popupEnigmas.style.display = "none";
+	}
+	span2.onclick = function() {
+	    popupVideos.style.display = "none";
+	}
+	span3.onclick = function() {
+	    popupJogos.style.display = "none";
+	}
+
+	//Se o usuário clicar fora da tela do pop up, fecha o pop up também
+	window.onclick = function(event) {
+	    if (event.target == popupPalitos) {
+	        popupPalitos.style.display = "none";
+	    }
+	    if (event.target == popupEnigmas) {
+	        popupEnigmas.style.display = "none";
+	    }
+	    if (event.target == popupVideos) {
+	        popupVideos.style.display = "none";
+	    }
+	    if (event.target == popupJogos) {
+	        popupJogos.style.display = "none";
+	    }
+	}
+};
